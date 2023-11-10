73d292a0ad1b67527af7c727ee6ad0bb12754b51
Pradnya2203 <pradnyashimpi22@gmail.com>
2023-11-10 16:34:53 +0530

    Chatbot

---
+++ helper/openai_api.py
@@ -0,0 +1,59 @@
+import os
+
+import openai
+# from dotenv import load_dotenv
+# load_dotenv()
+
+# openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
+openai.api_key =  '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'
+
+
+
+def chat_complition(prompt: str) -> dict:
+    '''
+    Call Openai API for text completion
+    Parameters:
+        - prompt: user query (str)
+    Returns:
+        - dict
+    '''
+    try:
+        completion = openai.ChatCompletion.create(
+          model="gpt-3.5-turbo",
+          messages=[
+            {"role": "system", "content": f'{prompt}'}
+          ]
+        )
+
+        answer=completion.choices[0].message['content']
+        # response = openai.Completion.create(
+        #     model='gpt-3.5-turbo',
+        #     engine='text-davinci-003',
+        #     prompt=f'Human: {prompt}\nAI: ',
+        #     temperature=0.9,
+        #     max_tokens=150,
+        #     top_p=1,
+        #     frequency_penalty=0,
+        #     presence_penalty=0.6,
+        #     stop=['Human:', 'AI:']
+        # )
+
+        # response = openai.ChatCompletion.create(
+        #     model='gpt-3.5-turbo',
+        #     messages=[
+        #         {'role': 'system', 'content': 'You are a helpful assistant.'},
+        #         {'role': 'user', 'content': prompt},
+        #     ]
+        # )
+        print(answer)
+        return {
+            'status': 1,
+            'response': (f'{answer}')
+        }
+    except:
+        return {
+            'status': 0,
+            'response': ''
+        }
---
+++ src/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+
+from helper.openai_api import chat_complition
+from helper.twilio_api import send_message
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return jsonify(
+        {
+            'status': 'OK',
+            'wehook_url': 'BASEURL/twilio/receiveMessage',
+            'message': 'The webhook is ready.',
+            'video_url': 'https://youtu.be/y9NRLnPXsb0'
+        }
+    )
+
+
+@app.route('/twilio/receiveMessage', methods=['POST'])
+def receiveMessage():
+    try:
+        # Extract incomng parameters from Twilio
+        message = request.form['Body']
+        sender_id = request.form['From']
+        # Get response from Openai
+        print("hey")
+        result = chat_complition(message)
+        print(result)
+        if result['status'] == 1:
+            send_message(sender_id, result['response'])
+    except:
+        pass
+    return 'OK', 200
